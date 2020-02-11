64a1481e0396ecf630fe19252b83c4f7b269844e
adesombre <56715323+adesombre@users.noreply.github.com>
2020-02-11 16:52:29 +0100

    correction fichier labyrhinte,ajout de map.txt

--- labyrhinte.py
+++ labyrhinte.py
@@ -1,31 +1,14 @@
-def labyrinthe():                                                                               
-    labyrinthe=["################################################",
-                "#    #              #                          #", 
-                "#    #              #                          #",
-                "#    #              #                          #",
-                "#    ##########              ########          #",
-                "#                                              #",
-                "#                       ############           #",            #",
-                "#                                  #           #",
-                "#                 ############     #           #",
-                "######            #                #           #",
-                "#                 #            #################",
-                "#                 #                            #", 
-                "#    #            #                            #",
-                "#    #       ############          #############",
-                "#    #                  #                      #",
-                "#    #                  #                      #",
-                "#                       #        #             #",
-                "#                                #             #",
-                "############                     #             #",
-                "#                                #             #",
-                "################################################"
-               ]
-    for i in labyrinthe:
-       print(i)
+# fonction  affichage du labyrhinte
+def charge_labyrinthe():
+    labyrinthe = open("map.txt")# Ici, ouvrir le fichier
+
+    # affiche labyrinthe
+def affiche_labyrinthe():
+    labyrinthe = open("map.txt")
+    for ligne in labyrinthe.readlines():
+        print(ligne)
 
 
-
-
-
-labyrinthe()
+# programme principal
+charge_labyrinthe()
+affiche_labyrinthe()
