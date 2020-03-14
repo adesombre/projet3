9ee4cc077900986fde4e5fa66af19d6c11340f88
adesombre <alaindesombre@gmail.com>
2020-03-14 16:25:24 +0100

    prise des objets avancee

--- labyrinthe.py
+++ labyrinthe.py
@@ -42,47 +42,49 @@ def objet(labyrinthe):
 def main():
     labyrinthe, x, y = load_labyrinthe()
     objet(labyrinthe)
+
     while True:
         display_labyrinthe(labyrinthe)
         user_input = input("appuyer sur un touche!")
-        p=0
+        point = 0
+        point = point + 1
         if user_input == 't':
             # y - 1 est la case au dessus
             if labyrinthe[y - 1][x] != '#':
-                if 'm' != objet(labyrinthe):
+                if 'm' != object:
 
-                    print("point")
-
+                    print("point",point)
                 labyrinthe[y - 1][x] = 'm'
                 labyrinthe[y][x] = ' '
                 y = y - 1
+                point = point + 1
         elif user_input == 'r':
             if labyrinthe[y][x + 1] != '#':
-                if 'm' != objet(labyrinthe):
-                    print("point")
+                if 'm' != object:
+
+                    print("point",point)
                 labyrinthe[y][x + 1] = 'm'
                 labyrinthe[y][x] = ' '
                 x = x + 1
+                point = point + 1
         elif user_input == 'l':
             if labyrinthe[y][x - 1] != '#':
-                if 'm' != objet(labyrinthe):
-                    print("point")
+                if 'm' != object:
+
+                    print("point",point)
                 labyrinthe[y][x - 1] = 'm'
                 labyrinthe[y][x] = ' '
                 x = x - 1
+                point = point + 1
         elif user_input == 'b':
             if labyrinthe[y + 1][x] != '#':
-                if 'm' != objet(labyrinthe):
-                    print("point")
+                if 'm' != object:
+
+                    print("point",point)
                 labyrinthe[y + 1][x] = 'm'
                 labyrinthe[y][x] = ' '
                 y = y + 1
+                point =point + 1
 
 
 main()
-def prend_objet():
-    point=0
-    if 'm' != objet(labyrinthe):
-        print("point")
-        point = point +1
-    return point
