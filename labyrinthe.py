f4190bd47cda67ece37f6864696d77561afd0457
adesombre <alaindesombre@gmail.com>
2020-03-25 17:02:11 +0100

    ajout de fonction

--- labyrinthe.py
+++ labyrinthe.py
@@ -33,23 +33,28 @@ def objet(labyrinthe):
     for o in object:
         xo = random.randint(0, 14)
         yo = random.randint(0, 14)
-        while labyrinthe[yo][xo] == "#":
+        while labyrinthe[yo][xo] != " ":
             xo = random.randint(0, 14)
             yo = random.randint(0, 14)
         labyrinthe[yo][xo] = o
 
 
-def deplacement(user_input, y, x):
-    labyrinthe, y, x = load_labyrinthe()
-    point = 0
-    if user_input == user_input:
-        # y - 1 est la case au dessus
-        if labyrinthe[y][x] != '#':
-            if labyrinthe[y][x] != " ":
-                if labyrinthe[y][x] == "g":
-                    end_game(point)
-                    point = point + 1
-                    print(point)
+def check_move(lab, new_y, new_x):
+    if 0 <= new_y < len(lab) and 0 <= new_x < len(lab[new_y]) and \
+            lab[new_y][new_x] != "#":
+        return True
+    else:
+        return False
+
+
+def collect_item(y, x, lab, point):
+    if lab[y][x] != " ":
+        if lab[y][x] == "g":
+            end_game(point)
+        else:
+            point = point + 1
+            print(point)
+    return point
 
 
 def end_game(point):
@@ -64,31 +69,34 @@ def main():
 
     labyrinthe, y, x = load_labyrinthe()
     objet(labyrinthe)
-
     point = 0
     while True:
         display_labyrinthe(labyrinthe)
         user_input = input("appuyer sur un touche!")
         if user_input == 't':
-            deplacement(user_input, y - 1, x)
-            labyrinthe[y - 1][x] = 'm'
-            labyrinthe[y][x] = ' '
-            y = y - 1
+            if check_move(labyrinthe, y - 1, x):
+                point = collect_item(y - 1, x, labyrinthe, point)
+                labyrinthe[y - 1][x] = 'm'
+                labyrinthe[y][x] = ' '
+                y = y - 1
         elif user_input == 'r':
-            deplacement(user_input, y, x + 1)
-            labyrinthe[y][x + 1] = 'm'
-            labyrinthe[y][x] = ' '
-            x = x + 1
+            if check_move(labyrinthe, y, x + 1):
+                point = collect_item(y, x + 1, labyrinthe, point)
+                labyrinthe[y][x + 1] = 'm'
+                labyrinthe[y][x] = ' '
+                x = x + 1
         elif user_input == 'b':
-            deplacement(user_input, y + 1, x)
-            labyrinthe[y + 1][x] = 'm'
-            labyrinthe[y][x] = ' '
-            y = y + 1
+            if check_move(labyrinthe, y + 1, x):
+                point = collect_item(y + 1, x, labyrinthe, point)
+                labyrinthe[y + 1][x] = 'm'
+                labyrinthe[y][x] = ' '
+                y = y + 1
         elif user_input == 'l':
-            deplacement(user_input, y, x - 1)
-            labyrinthe[y][x - 1] = 'm'
-            labyrinthe[y][x] = ' '
-            x = x - 1
+            if check_move(labyrinthe, y, x - 1):
+                point = collect_item(y, x - 1, labyrinthe, point)
+                labyrinthe[y][x - 1] = 'm'
+                labyrinthe[y][x] = ' '
+                x = x - 1
 
 
 
